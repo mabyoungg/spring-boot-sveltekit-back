7cba3c4cd449cb257fecbb1fd356dff27caf9f11
mabyoungg <mabyoungg@gmail.com>
2024-05-01 19:38:55 +0900

    feat: springdoc config 추가

---
+++ src/main/java/org/example/springbootsveltekitback/global/springDoc/SpringDocConfig.java
@@ -0,0 +1,10 @@
+package org.example.springbootsveltekitback.global.springDoc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "TEST 20240501 API", version = "v1"))
+public class SpringDocConfig {
+}
